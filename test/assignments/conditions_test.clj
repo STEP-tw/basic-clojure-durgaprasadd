dc1a11062cba28bfc2e217e4288458aaf4b35ac1
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2020-01-26 15:12:08 +0530

    Implemented harichandra
    Added test cases

--- test/assignments/conditions_test.clj
+++ test/assignments/conditions_test.clj
@@ -13,3 +13,18 @@
     (is (= 2 (informative-divide 4 2))))
   (testing "zero denominator"
     (is (= :infinite (informative-divide 3 0)))))
+
+(deftest harishchandra-test
+  (testing "truthy values"
+    (are [x y] (= x y)
+               true (harishchandra true)
+               2 (harishchandra 2)
+               0 (harishchandra 0)
+               )
+    )
+  (testing "falsy/nil values"
+    (are [x y] (= x y)
+               nil (harishchandra false)
+               nil (harishchandra nil))
+    )
+  )
